3a9a6f987cad2f331a01982932ff8943c7df303e
lenovo <lenovo@LAPTOP-2NKIQS9D.homenet.telecomitalia.it>
2021-06-12 09:29:27 +0200

    Aggiornato

--- src/main/java/it/polito/tdp/artsmia/FXMLController.java
+++ src/main/java/it/polito/tdp/artsmia/FXMLController.java
@@ -5,6 +5,8 @@
 package it.polito.tdp.artsmia;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import it.polito.tdp.artsmia.model.Model;
@@ -26,7 +28,7 @@ public class FXMLController {
     private URL location;
 
     @FXML // fx:id="boxLUN"
-    private ChoiceBox<?> boxLUN; // Value injected by FXMLLoader
+    private ChoiceBox<Integer> boxLUN; // Value injected by FXMLLoader
 
     @FXML // fx:id="btnCalcolaComponenteConnessa"
     private Button btnCalcolaComponenteConnessa; // Value injected by FXMLLoader
@@ -47,12 +49,44 @@ public class FXMLController {
     
     @FXML
     void doAnalizzaOggetti(ActionEvent event) {
+    	model.creaGrafo();
+    	this.txtObjectId.setEditable(true);
+    	this.btnCalcolaComponenteConnessa.setDisable(false);
 
     }
 
     @FXML
     void doCalcolaComponenteConnessa(ActionEvent event) {
-
+    	this.boxLUN.getItems().clear();
+    	this.txtResult.clear();
+    	int id;
+    	try {
+    		id = Integer.parseInt(this.txtObjectId.getText());
+    		if(model.getIdMap().get(id)!=null)
+    			model.componenteConnessa(id);
+    		else
+    			throw new NullPointerException();
+    	}catch(NumberFormatException e) {
+    		this.txtResult.setText("Inserire un numero");
+    		return;
+    	}catch(NullPointerException e) {
+    		this.txtResult.setText("Oggetto inesistente");
+    		return;
+    	}
+    	this.txtResult.setText("SOTTOGRAFO CREATO!\n"
+				+ "\t#VERTICI: "+model.getSottoGrafo().vertexSet().size());
+    	
+    	List<Integer> size = new ArrayList<>();
+    	
+    	for(int i=2; i<=model.getSottoGrafo().vertexSet().size(); i++) {
+    		if(i>15)
+    			break;
+    		size.add(i);
+    	}
+    	
+    	this.boxLUN.setDisable(false);
+    	this.btnCercaOggetti.setDisable(false);
+    	this.boxLUN.getItems().addAll(size);
     }
 
     @FXML
@@ -62,6 +96,10 @@ public class FXMLController {
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+    	this.txtObjectId.setEditable(false);
+    	this.btnCalcolaComponenteConnessa.setDisable(true);
+    	this.boxLUN.setDisable(true);
+    	this.btnCercaOggetti.setDisable(true);
         assert boxLUN != null : "fx:id=\"boxLUN\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnCalcolaComponenteConnessa != null : "fx:id=\"btnCalcolaComponenteConnessa\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnCercaOggetti != null : "fx:id=\"btnCercaOggetti\" was not injected: check your FXML file 'Scene.fxml'.";
--- src/main/java/it/polito/tdp/artsmia/model/Model.java
+++ src/main/java/it/polito/tdp/artsmia/model/Model.java
@@ -1,5 +1,110 @@
 package it.polito.tdp.artsmia.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+
+import it.polito.tdp.artsmia.db.ArtsmiaDAO;
+
 public class Model {
+	
+	private Graph<ArtObject, DefaultWeightedEdge> grafo;
+	ArtsmiaDAO dao;
+	private Map<Integer, ArtObject> idMap; 
+	private Graph<ArtObject, DefaultWeightedEdge> sottoGrafo;
+	
+	public Map<Integer, ArtObject> getIdMap() {
+		return idMap;
+	}
+
+	public Model() {
+		dao = new ArtsmiaDAO();
+		idMap = new HashMap<>();
+	}
+	
+	public void creaGrafo() {
+		grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		dao.listObjects(idMap);
+		Graphs.addAllVertices(grafo, idMap.values());
+		
+		for(Adiacenza a: dao.getAdiacenze()) {
+				Graphs.addEdge(grafo, idMap.get(a.getId1()), idMap.get(a.getId2()), a.getPeso());
+		}
+		
+		/*System.out.println("GRAFO CREATO!\n"
+				+ "\t#VERTICI: "+grafo.vertexSet().size()+"\n"
+				+ "\t#ARCHI: "+grafo.edgeSet().size());*/
+	}
+	
+	public void componenteConnessa(int id){
+		
+		sottoGrafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		sottoGrafo.addVertex(idMap.get(id));
+		
+		BreadthFirstIterator<ArtObject, DefaultWeightedEdge> it = new BreadthFirstIterator<>(grafo, idMap.get(id));
+		
+		it.addTraversalListener(new TraversalListener<ArtObject, DefaultWeightedEdge>() {
+			
+			@Override
+			public void vertexTraversed(VertexTraversalEvent<ArtObject> e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void vertexFinished(VertexTraversalEvent<ArtObject> e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void edgeTraversed(EdgeTraversalEvent<DefaultWeightedEdge> e) {
+				// TODO Auto-generated method stub
+				DefaultWeightedEdge edge = e.getEdge();
+				ArtObject source = grafo.getEdgeSource(edge);
+				ArtObject target = grafo.getEdgeTarget(edge);
+				
+				sottoGrafo.addVertex(source);
+				sottoGrafo.addVertex(target);
+
+				Graphs.addEdge(sottoGrafo, source, target, grafo.getEdgeWeight(edge));
+				
+			}
+			
+			@Override
+			public void connectedComponentStarted(ConnectedComponentTraversalEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void connectedComponentFinished(ConnectedComponentTraversalEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		while(it.hasNext())
+			it.next();
+		
+		/*System.out.println("\nSOTTOGRAFO CREATO!\n"
+				+ "\t#VERTICI: "+sottoGrafo.vertexSet().size()+"\n"
+				+ "\t#ARCHI: "+sottoGrafo.edgeSet().size());*/		
+		
+		
+	}
+
+	public Graph<ArtObject, DefaultWeightedEdge> getSottoGrafo() {
+		return sottoGrafo;
+	}
 
 }
